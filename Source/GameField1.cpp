63f4b35b4a1f89035ddf06876965c8885892aecd
mshaheen1 <majedshaheen1@gmail.com>
2015-07-28 14:34:02 -0500

    Create GameField1.cpp

---
+++ Source/GameField1.cpp
@@ -0,0 +1,12 @@
+GameField1(int width,int height,int n,int nblue,int nred)
+{
+this->width=width;
+this->height=height;
+turn=false;
+this->nummoves=n;
+this->nred=nred;
+this->nblue=nblue;
+
+
+
+}
