117692b5cfe3786ff7703ed45b30490fbe9dcacf
Robert Hernandez <rhernandez513@gmail.com>
2015-08-01 04:36:57 -0500

    update container file to stay with style & pro

--- Headers/Containers.h
+++ Headers/Containers.h
@@ -3,12 +3,12 @@
 #include <iostream>
 
 namespace Ants {
-struct Position {
-  int x;
-  int y;
-};
-enum Direction { LEFT, RIGHT, UP, DOWN };
-enum Color { red, blue };
+struct Position { int x; int y; };
+enum class Direction { LEFT, RIGHT, UP, DOWN };
+enum class Color { red, blue };
 std::ostream& operator<<(std::ostream& os, Color color);
+
+// Flags checking if the queen has died.
+static bool red_queen_is_dead = false, blue_queen_is_dead = false;
 }
-#endif // CONTAINERS_H
+#endif  // CONTAINERS_H
---
+++ Source/Containers.cpp
@@ -0,0 +1,28 @@
+#include "..\\Headers\\Containers.h"
+#include "..\\Headers\\Ant.h"
+#include <iostream>
+
+using namespace Ants;
+
+std::ostream& Ants::operator<<(std::ostream& os, Color color) {
+  switch (color) {
+    case (Color::red):
+      os << "Red";
+      break;
+    case (Color::blue):
+      os << "Blue";
+      break;
+    default:
+      os << "";
+      break;
+  }
+  return os;
+}
+
+// Extensible Storage
+struct GameBlock {
+  bool isFilled = false;
+  Ant* _ant1 = nullptr;
+  Ant* _ant2 = nullptr;
+  Position _pos;
+};
